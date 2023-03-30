20fb695e763e883e9db97d472732447e30a64170
sNishant011 <shresthanishant011@gmail.com>
2023-03-30 10:03:09 +0545

    chore: tailwind css setup

---
+++ src/layouts/AdminLayout.tsx
@@ -0,0 +1,47 @@
+import { Link, Outlet } from "react-router-dom";
+
+const AdminLayout = () => {
+  return (
+    <div>
+      <div className="drawer drawer-mobile">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          <div>
+            <label
+              htmlFor="my-drawer-2"
+              className="btn btn-primary drawer-button lg:hidden"
+            >
+              <span className="sr-only">Open sidebar</span>
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  clipRule="evenodd"
+                  fillRule="evenodd"
+                  d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+                ></path>
+              </svg>
+            </label>
+          </div>
+          <main>
+            <Outlet />
+          </main>
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+            <li>
+              <Link to="/">Dashboard</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminLayout;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,8 @@
 import { lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
+import AdminLayout from "@/layouts/AdminLayout";
+
 // lazy import components
 const PostList = lazy(() => import("@/posts/list"));
 const CreatePost = lazy(() => import("@/posts/create"));
@@ -8,11 +10,17 @@ const CreatePost = lazy(() => import("@/posts/create"));
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (
-      <Suspense fallback={<div>Loading...</div>}>
-        <PostList />
-      </Suspense>
-    ),
+    element: <AdminLayout />,
+    children: [
+      {
+        index: true,
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <PostList />
+          </Suspense>
+        ),
+      },
+    ],
   },
   {
     path: "/create",
